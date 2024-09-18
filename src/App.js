57b717718421255525064c8280367b71c36c6f27
sowRaj2023 <sownyuvan1180@gmail.com>
2024-09-18 12:31:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,42 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
-import LoginWrapper from './components/LoginWrapper';
-import StudentList from './components/StudentList';
-import TeacherList from './components/TeacherList';
-import AddStudent from './components/AddStudent';
-import AddTeacher from './components/AddTeacher';
-import './App.css';
+import React from 'react';
+import TrafficProvider from './context/TrafficProvider'; // Adjust path if necessary
+import TrafficLight from './components/TrafficLight/TrafficLight';
+import EmergencyButton from './components/EmergencyButton/EmergencyButton';
+import PedestrianButton from './components/PedestrianButton/PedestrianButton';
+import TimerDisplay from './components/TimerDisplay/TimerDisplay';
+import './App.css'; // Optional: Include any global styles here
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-    };
-  }
-
-  setUser = (user) => {
-    this.setState({ user });
-  };
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          {this.state.user && (
-            <nav>
-              <ul>
-                <li><Link to="/students">Student List</Link></li>
-                <li><Link to="/add-student">Add Student</Link></li>
-                <li><Link to="/teachers">Teacher List</Link></li>
-                <li><Link to="/add-teacher">Add Teacher</Link></li>
-              </ul>
-            </nav>
-          )}
-          <Routes>
-            <Route path="/login" element={<LoginWrapper setUser={this.setUser} />} />
-            {this.state.user ? (
-              <>
-                <Route path="/students" element={<StudentList />} />
-                <Route path="/add-student" element={<AddStudent />} />
-                <Route path="/teachers" element={<TeacherList />} />
-                <Route path="/add-teacher" element={<AddTeacher />} />
-                <Route path="*" element={<Navigate to="/students" />} />
-              </>
-            ) : (
-              <Route path="*" element={<Navigate to="/login" />} />
-            )}
-          </Routes>
+function App() {
+  return (
+    <TrafficProvider>
+      <div className="app">
+        <TimerDisplay />
+        <TrafficLight />
+        <div className="button-container">
+          <PedestrianButton />
+          <EmergencyButton />
         </div>
-      </Router>
-    );
-  }
+      </div>
+    </TrafficProvider>
+  );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/context/TrafficContext.js
@@ -0,0 +1,8 @@
+import React, { createContext } from 'react';
+
+// Create the context
+export const TrafficContext = createContext();
+
+
+
+
